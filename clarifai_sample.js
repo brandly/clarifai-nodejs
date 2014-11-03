a95adb6b25d40c405085c9d35bc590baf6a8c80d
jimreesman <jim@clarifai.com>
2014-11-03 13:17:21 -0500

    commit correct file

--- clarifai_sample.js
+++ clarifai_sample.js
@@ -1,38 +1,64 @@
 // node_example.js - Example showing use of Clarifai node.js API
 
-
 var Clarifai = require('./clarifai_node.js');
+Clarifai.initAPI(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
 
+// include unit test utilities
+var assert = require("assert");
+var stdio = require('stdio');
+
+// support some command-line options
+var opts = stdio.getopt( {
+	'print-results' : { description: 'print results'},
+	'print-http' : { description: 'print HTTP requests and responses'},
+	'host' : { key : 'h', args:1, description: 'specify an alternate host'},
+	'verbose' : { key : 'v', description: 'verbose output'}
+});
+var verbose = opts["verbose"];
+
+Clarifai.setVerbose( verbose );
+if( "host" in opts ) {
+	Clarifai.setHost( opts["host"] );
+}
+if( opts["print-http"] ) {
+	Clarifai.setLogHttp( true ) ;
+}
+
+if(verbose) console.log("using CLIENT_ID="+Clarifai._clientId+", CLIENT_SECRET="+Clarifai._clientSecret);
 
 // exampleTagSingleURL() shows how to request the tags for a single image URL
-function exampleTagSingleURL() {
-	var exampleImageURL = 'http://www.clarifai.com/img/metro-north.jpg';
-	Clarifai.tagURL( exampleImageURL , function(res) {
-		console.log(res);
-		// get the docid
-		console.log(res["results"][0]["docid"]);
-	} );		
+function exampleTagSingleURL() {var testImageURL = 'http://www.clarifai.com/img/metro-north.jpg';
+	var ourId = "train station 1"; // this is any string that identifies the image to your system
+
+	Clarifai.tagURL( testImageURL , null, function( res, localId ) {
+		if( opts["print-results"] ) {
+			console.log( res, localId );
+		};
+	} );
 }
 
 // exampleTagMultipleURL() shows how to request the tags for multiple images URLs
 function exampleTagMultipleURL() {
-	var exampleImageURLs = [ 
+	var testImageURLs = [ 
 	"http://www.clarifai.com/img/metro-north.jpg", 
 	"http://www.clarifai.com/img/img_fire_bg.jpg" ] ;
+	var ourIds =  [ "train station 1", 
+	                        "img002032" ]; // this is any string that identifies the image to your system
 
-	Clarifai.tagURLmulti( exampleImageURLs, function( res ){
-		console.log(res);
-		// get the docids
-		console.log(res["results"][0]["docid"]);
-		console.log(res["results"][1]["docid"]);
-	} );		
+	Clarifai.tagURL( testImageURLs , null, function( res, localId ) {
+		if( opts["print-results"] ) {
+			console.log( res , localId );
+		};
+	} );
 }
 
 // exampleFeedback() shows how to send feedback (add or remove tags) from 
 // a list of docids. Recall that the docid uniquely identifies an image previously
 // presented for tagging to one of the tag methods.
 function exampleFeedback() {
-	var docids = [
+// these are docids that just happen to be in the database right now. this test should get 
+// upgraded to tag images and use the returned docids.
+var docids = [
 	"15512461224882630000",
 	"9549283504682293000"
 	];
@@ -40,13 +66,23 @@ function exampleFeedback() {
 	"addTag1",
 	"addTag2"
 	];
-	Clarifai.feedbackAddTagsToDocids( docids, addTags, console.log )
+	Clarifai.feedbackAddTagsToDocids( docids, addTags, null, function( res ) {
+		if( opts["print-results"] ) {
+			console.log( res );
+		};
+		assert.equal( res["status_code"], "OK" );
+	} );
 
 	var removeTags = [
 	"removeTag1",
 	"removeTag2"
 	];
-	Clarifai.feedbackRemoveTagsFromDocids( docids, removeTags, console.log )
+	Clarifai.feedbackRemoveTagsFromDocids( docids, removeTags, null, function( res ) {
+		if( opts["print-results"] ) {
+			console.log( res );
+		};
+		assert.equal( res["status_code"], "OK" );
+	} );
 }
 
 
